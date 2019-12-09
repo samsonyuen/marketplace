28a2ae92580c722201f3f9b2eb748353c8fa96eb
Samson Yuen <samsony@outlook.com>
2019-12-08 17:58:03 -0800

    homepage menu with placeholder items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
+
 
 function App() {
   return (
     <div>
-
+        <HomePage />
     </div>
   );
 }
